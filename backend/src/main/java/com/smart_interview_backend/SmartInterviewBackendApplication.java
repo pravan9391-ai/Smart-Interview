0da6962e62d435a148875c778f7a2c4cdf2661d1
pravan <pravan9391@gmmail.com>
2026-08-12 23:15:55 +0530

    Initial project setup

---
+++ backend/src/main/java/com/smart_interview_backend/SmartInterviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart_interview_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInterviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInterviewBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smart_interview_backend/SmartInterviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_interview_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInterviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
